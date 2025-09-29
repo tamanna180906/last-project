b01bd573bfe789b306d98beb68875e2cf52d4fab
tamanna180906 <myeuphoria330@gmail.com>
2025-09-29 14:29:07 +0600

    add qun

--- src/components/slice/productSlice.js
+++ src/components/slice/productSlice.js
@@ -18,14 +18,39 @@ export const productSlice = createSlice({
         state.cartItem=[...state.cartItem, action.payload]
       }     
     },
+     increaseQun: (state, action) => {
+      let findindex = state.cartItem.findIndex(
+        (item) => item.id === action.payload.id
+      )
+      if (findindex !== -1) {
+        state.cartItem[findindex].qun++
+      }
+    },
+
+    decreaseQun: (state, action) => {
+      let findindex = state.cartItem.findIndex(
+        (item) => item.id === action.payload.id
+      )
+      if (findindex !== -1) {
+        if (state.cartItem[findindex].qun > 1) {
+          state.cartItem[findindex].qun--
+        } else {
+          // যদি quantity = 1 থাকে → remove করে দাও
+          state.cartItem = state.cartItem.filter(
+            (item) => item.id !== action.payload.id
+          )
+        }
+      }
+    },
     removeCart:(state,action)=>{
      let filtercart=state.cartItem.filter((item)=>item.id !=action.payload.id) 
      state.cartItem=filtercart
+    
     }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addToCart,removeCart } = productSlice.actions
+export const { addToCart,increaseQun, decreaseQun, removeCart } = productSlice.actions
 
 export default productSlice.reducer
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { removeCart } from '../components/slice/productSlice'
+import { decreaseQun, increaseQun, removeCart } from '../components/slice/productSlice'
 
 
 function Cart() {
-    let dispatch=useDispatch()
+    let dispatch = useDispatch()
 
     let cartData = useSelector((state) => state.product.cartItem)
 
-    let handleCartItemRemove=(item)=>{
-        let result= confirm("Are You Sure")
+    let handleCartItemRemove = (item) => {
+        let result = confirm("Are You Sure")
 
-        if(result){
-        dispatch(removeCart(item))
+        if (result) {
+            dispatch(removeCart(item))
         }
-        
+
     }
 
 
@@ -30,116 +30,119 @@ function Cart() {
                         <div className="space-y-6">
                             {cartData.length > 0
                                 ?
-                                cartData.map((item)=>(
-                                <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700  md:p-6">
-                                    <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-                                        <a href="#" className="shrink-0 md:order-1">
-                                            <img
-                                                className="hidden h-20 w-20 dark:block"
-                                                src={item.thumbnail}
-                                                alt="imac image"
-                                            />
-                                        </a>
-                                        <label htmlFor="counter-input" className="sr-only">
-                                            Choose quantity:
-                                        </label>
-                                        <div className="flex items-center justify-between md:order-3 md:justify-end">
-                                            <div className="flex items-center">
-                                                <button
-                                                    type="button"
-                                                    id="decrement-button"
-                                                    data-input-counter-decrement="counter-input"
-                                                    className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-                                                >
-                                                    <svg
-                                                        className="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                                                        aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 18 2"
-                                                    >
-                                                        <path
-                                                            stroke="currentColor"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth={2}
-                                                            d="M1 1h16"
-                                                        />
-                                                    </svg>
-                                                </button>
-                                                <input
-                                                    type="text"
-                                                    id="counter-input"
-                                                    data-input-counter=""
-                                                    className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-black"
-                                                    placeholder=""
-                                                    defaultValue={item.qun}
-                                                    required=""
+                                cartData.map((item) => (
+                                    <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700  md:p-6">
+                                        <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                                            <a href="#" className="shrink-0 md:order-1">
+                                                <img
+                                                    className="hidden h-20 w-20 dark:block"
+                                                    src={item.thumbnail}
+                                                    alt="imac image"
                                                 />
-                                                <button
-                                                    type="button"
-                                                    id="increment-button"
-                                                    data-input-counter-increment="counter-input"
-                                                    className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-                                                >
-                                                    <svg
-                                                        className="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                                                        aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 18 18"
-                                                    >
-                                                        <path
-                                                            stroke="currentColor"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth={2}
-                                                            d="M9 1v16M1 9h16"
-                                                        />
-                                                    </svg>
-                                                </button>
-                                            </div>
-                                            <div className="text-end md:order-4 md:w-32">
-                                                <p className="text-base font-bold text-gray-900 dark:text-white">
-                                                    $1,499
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-                                            <a
-                                                href="#"
-                                                className="text-base font-medium text-gray-900 hover:underline dark:text-black"
-                                            >
-                                                {item.title}
                                             </a>
-                                            <div className="flex items-center gap-4">
-                                                <button onClick={()=>handleCartItemRemove(item)}
-                                                    type="button"
-                                                    className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
-                                                >
-                                                    <svg
-                                                        className="me-1.5 h-5 w-5"
-                                                        aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        width={24}
-                                                        height={24}
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
+                                            <label htmlFor="counter-input" className="sr-only">
+                                                Choose quantity:
+                                            </label>
+                                            <div className="flex items-center justify-between md:order-3 md:justify-end">
+                                                <div className="flex items-center">
+                                                    <button
+                                                        type="button"
+                                                        id="decrement-button"
+                                                        onClick={() => dispatch(decreaseQun(item))}
+                                                        data-input-counter-decrement="counter-input"
+                                                        className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
                                                     >
-                                                        <path
-                                                            stroke="currentColor"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth={2}
-                                                            d="M6 18 17.94 6M18 18 6.06 6"
-                                                        />
-                                                    </svg>
-                                                    Remove
-                                                </button>
+                                                        <svg
+                                                            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                                            aria-hidden="true"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 18 2"
+                                                        >
+                                                            <path
+                                                                stroke="currentColor"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth={2}
+                                                                d="M1 1h16"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                    <input
+                                                        type="text"
+                                                        id="counter-input"
+                                                        data-input-counter=""
+                                                        className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-black"
+                                                        placeholder=""
+                                                        value={item.qun}
+                                                        readOnly
+                                                        required=""
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        id="increment-button"
+                                                        onClick={() => dispatch(increaseQun(item))}
+                                                        data-input-counter-increment="counter-input"
+                                                        className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                                                    >
+                                                        <svg
+                                                            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                                            aria-hidden="true"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 18 18"
+                                                        >
+                                                            <path
+                                                                stroke="currentColor"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth={2}
+                                                                d="M9 1v16M1 9h16"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                                <div className="text-end md:order-4 md:w-32">
+                                                    <p className="text-base font-bold text-gray-900 dark:text-white">
+                                                        $1,499
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                                                <a
+                                                    href="#"
+                                                    className="text-base font-medium text-gray-900 hover:underline dark:text-black"
+                                                >
+                                                    {item.title}
+                                                </a>
+                                                <div className="flex items-center gap-4">
+                                                    <button onClick={() => handleCartItemRemove(item)}
+                                                        type="button"
+                                                        className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                                                    >
+                                                        <svg
+                                                            className="me-1.5 h-5 w-5"
+                                                            aria-hidden="true"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            width={24}
+                                                            height={24}
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                        >
+                                                            <path
+                                                                stroke="currentColor"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth={2}
+                                                                d="M6 18 17.94 6M18 18 6.06 6"
+                                                            />
+                                                        </svg>
+                                                        Remove
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
                                 ))
                                 :
                                 <h2>No Cart Items Found</h2>
